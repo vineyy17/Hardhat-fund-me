51b6f3706fe8ba0b2e6202a1b7acfdd4fc8b91f0
Divine <officialkb17@gmail.com>
2024-02-03 08:53:40 -0800

    initial project config

---
+++ hardhat.config.js
@@ -0,0 +1,45 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy")
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0xkey'
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key'
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || 'key'
+
+
+module.exports = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL || "",
+      accounts: [],
+      // chainId: 11155111,
+    },
+    // localhost: {
+    //   url: 'http://127.0.0.1:8545/',
+    //   chainId: 31337,
+
+    // }
+  },
+  solidity: "0.8.8",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: 'USD',
+    // coinmarketcap: COINMARKETCAP_API_KEY
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    users: {
+      default: 1
+    }
+  }
+};
